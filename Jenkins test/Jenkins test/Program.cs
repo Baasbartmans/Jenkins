10e6d45ca80853462aabbc3ebd541dd1c547b657
LAPTOP-9PR1HF00\bkolb <b.kolb@live.nl>
2017-12-11 11:47:51 +0100

    De commit

---
+++ Jenkins test/Jenkins test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Ja");
+            }
+        }
+    }
+}
